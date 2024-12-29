a635e4fae2112b9423869da5135c819b801f6eaa
Murtadha Almoamen <murtadhaalmoamen8@gmail.com>
2024-12-29 18:38:57 +0300

    setup

---
+++ server.js
@@ -0,0 +1,23 @@
+require("dotenv").config();;
+require('./config/database');
+
+const express = require("express");
+const app = express();
+
+const methodOverride = require("method-override");
+const morgan = require("morgan");
+
+// Set the port from environment variable or default to 3000
+const port = process.env.PORT ? process.env.PORT : "3000";
+
+
+// Middleware to parse URL-encoded data from forms
+app.use(express.urlencoded({ extended: false }));
+// Middleware for using HTTP verbs such as PUT or DELETE
+app.use(methodOverride("_method"));
+// Morgan for logging HTTP requests
+app.use(morgan('dev'));
+
+app.listen(port, () => {
+  console.log(`The express app is ready on port ${port}!`);
+});
